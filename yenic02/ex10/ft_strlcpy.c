58305d6ec9ffae2435948b6c99b34151ac4d91ac
Seydi Ali İçlek <93774058+Themilkshake@users.noreply.github.com>
2023-09-19 12:07:40 +0300

    Add files via upload

---
+++ yenic02/ex10/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: siclek <siclek@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 21:40:07 by siclek            #+#    #+#             */
+/*   Updated: 2023/09/18 21:41:50 by siclek           ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	c;
+	unsigned int	i;
+
+	c = 0;
+	i = 0;
+	while (src[c] != '\0')
+	{
+		c++;
+	}
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (c);
+}
